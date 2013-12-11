6bc73bfc0477988849d563d8323e663fbf12ef68
samueldowens <samueldowens@gmail.com>
2013-12-11 17:09:46 -0500

    initial commit, added omniauth-facebook to gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FacebookFriendInterestMapper::Application.config.secret_key_base = '0ad17f1941b5c1bea174eda01e0d74403df9e9f9ad4150a846ed97ed92539edfeeeefb35fa773334a14c9c2c468e69a5ed65738b77840dc46559652502b5d7e6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FacebookFriendInterestMapper::Application.config.session_store :cookie_store, key: '_facebook_friend_interest_mapper_session'
