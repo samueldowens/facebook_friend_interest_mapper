c7bf61eead50ba69d7fa2b2189080d89c1b8adec
samueldowens <samueldowens@gmail.com>
2013-12-11 17:15:10 -0500

    added user migration

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :user do
+    provider "MyString"
+    uid "MyString"
+    name "MyString"
+    oauth_token "MyString"
+    oauth_expires_at "2013-12-11 17:14:47"
+  end
+end
