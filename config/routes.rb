faa1c46cbfccaa80867a966addfe7f836e6efc49
samueldowens <samueldowens@gmail.com>
2013-12-11 17:35:18 -0500

    added index as root in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ match 'auth/:provider/callback', to: 'sessions#create'
 match 'auth/failure', to: redirect('/')
 match 'signout', to: 'sessions#destroy', as: 'signout'
 
+root 'application#index'
+
 end
